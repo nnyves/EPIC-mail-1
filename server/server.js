900fb2c675c8dc6b2b7ce2c879e853c73af46e81
Ramadhan0 <nseorama@gmail.com>
2019-03-13 20:12:45 +0200

    set port

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.delete('/api/v1/email/:id', Email.delete);
 app.put('/api/v1/email/:id', Email.update);
 app.get('/api/v1/emails/status/:status', Email.getStatusEmail);
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
